a1ae905f929fb8b2fabaa9e43aa67b6d8ac087a1
drandreaskrueger <study@andreaskrueger.de>
2016-03-05 05:43:46 +0100

    windows results done

--- compare.py
+++ compare.py
@@ -12,42 +12,54 @@
 @thanks:   to Kevin Veroneau. I based parts of this on his code snippets. 
            http://www.pythondiary.com/blog/Feb.14,2012/too-many-micro-webframeworks.html
            
+           also see these memory measurements:
+           http://nuald.blogspot.de/2011/08/web-application-framework-comparison-by.html
 '''
 
 VERSION=                   "0.2.0"
 
-import os, time, sys, multiprocessing, subprocess
+import os, time, sys, multiprocessing, subprocess, threading
 import psutil # pip install psutil
+import requests # pip install requests
+
+from collections import Counter
+
 import test_bottle, test_webpy, test_flask, test_cherrypy, test_tornado
 
-def startProcessesTotallyIndependent(portstart=8000):
+def startProcessesTotallyIndependent(portstart=8000, stdout=sys.stdout, stderr=sys.stderr):
     """
     This one works. And the memory measurement is independent. Not like in 'obsolete.py'.
     
     This starts the processes as completely separate 'python name.py 8888' subprocesses.
     """ 
     
-    print "Start processes:"
+    print "Started independent subprocesses:", 
     processes=[]
     port=portstart
     sleepBetween=0.3
     
-    for module_name in ("test_bottle", "test_webpy", "test_flask", "test_cherrypy", "test_tornado"):
+    for framework in ("bottle", "webpy", "flask", "cherrypy", "tornado"):
         
-        p=subprocess.Popen(["python", module_name+".py", "%d" % port]) # start the process
+        module_name="test_%s" % framework
+        
+        # start the process
+        p=subprocess.Popen(["python", module_name+".py", "%d" % port], 
+                           stdout=stdout, stderr=stderr) 
         
         module=globals()[module_name]                                # like 'import test_bottle'
         v, url = getattr(module, 'version'), getattr(module, 'url')  # like 'test_bottle.url'
         
-        processes.append((p, "%s %s" % v(), url(port=port) )) # keep PID, and 
+        processes.append([ p, "%s %s" % v(), url(port=port)] ) # keep PID, and name, and url
         
-        # some of them need a bit longer to start:
-        if v()[0]=="cherrypy": time.sleep(1.5)  
-        elif v()[0]=="tornado": time.sleep(0.7) 
+        # some of them need a bit longer to start, sleepwait for clustered printing
+        if v()[0]=="cherrypy": time.sleep(1.0)  
+        elif v()[0]=="tornado": time.sleep(0.5) 
         else: time.sleep(sleepBetween)
         
+        print framework,
         port += 1 # continue with one-up
         
+    print
     return processes
         
 
@@ -63,8 +75,7 @@ def memory_usage(pid=None):
 def measureMemory(processes):
     sleep=2
     time.sleep(sleep)
-    print
-    print "Waited %s seconds, measuring now." % sleep
+    print "Waited %s seconds, measuring memory consumption now:" % sleep
     print 
     
     # 'posix', 'nt', 'os2', 'ce', 'java', 'riscos'.
@@ -81,6 +92,8 @@ def measureMemory(processes):
     print formatter % ("main "+VERSION, osname, os.getpid(), mem[0], mem[1], "")
     
 
+####################
+
 def killEm(processes):
     print "\nKilling PIDs:", 
     for p, name, _ in processes:
@@ -88,14 +101,88 @@ def killEm(processes):
         p.terminate()
     print
 
+####################
+
+def hammer_thread(url, results, timeout):
+    try:
+        response = requests.get(url, timeout=timeout)
+    except Exception as e:
+        # print type(e), e
+        results.append(-1)
+    else:
+        results.append(response.status_code)
+
+TIMEOUT=20
+    
+def hammer(url, rep, timeout=TIMEOUT):
+    T=time.time()
+    threads, results = [],[]
+    for _ in range(rep):
+        t=threading.Thread(target=hammer_thread, args=(url, results, timeout))
+        t.start()
+        threads.append(t)
+
+    for t in threads: t.join()
+    duration = time.time()-T
+    return duration, Counter(results)
     
 
-def measure2():
-    processes=startProcessesTotallyIndependent()
+REPETITIONS=10
+
+def test_hammer(rep=REPETITIONS):
+    url="http://www.google.de"
+    t,C=hammer(url, rep)
+    
+    rate=float(C[200])/sum(dict(C).values()) # percentage of http response 200 == OK
+    print "%5.1f seconds with %5.1f%% success rate - %s"% (t, 100*rate, C)
+
+def hammerLocalhost(processes, rep=REPETITIONS, timeout=TIMEOUT):
+    print"\nNow hammering each of those servers %d times, with a %.1f seconds timeout: " % (rep, timeout), 
+    time.sleep(1)
+    
+    results=[]
+    T=time.time()
+    
+    for _, name, url in processes:
+        m={}
+        m["name"], m["url"] = name, url
+        m["time"], m["Counter"] = hammer(url, rep, timeout=timeout)
+        results.append(m)
+        print m["name"].split(" ")[0], 
+        
+    print "- Done"
+    print "Concurrent hammering %d * %d urls took %4.1f secs." % (len(processes), rep, time.time() - T),
+    print "Results: (200 = OK; -1 = exception, probably 'requests.exceptions.ReadTimeout')"
+    print
+    
+    for m in results:
+        print "%-16s %40s" % (m["name"], m["url"]), 
+        rate=float(m["Counter"][200])/sum(dict(m["Counter"]).values())
+        print "  took %.4f seconds/call, with %5.1f%% success - %s"% (m["time"]/rep, 100*rate, m["Counter"])
+
+####################
+
+def measure2(rep=REPETITIONS, timeout=TIMEOUT):
+    f=open("logging.txt", "w")
+    stdout=stderr=f
+    
+    processes=startProcessesTotallyIndependent(stdout=stdout, stderr=stderr)
+    
     measureMemory(processes)
+    
+    hammerLocalhost(processes, rep=rep, timeout=timeout)
+    
+    for i in range(len(processes)):processes[i][2]="" # we don't need the urls anymore
+    
+    print "\nNow after all those calls, measure memory again ... ", 
+    measureMemory(processes)
+    
     killEm(processes)
+    f.close()
     
 if __name__ == '__main__':
-    measure2()
+    # test_hammer()
+    
+    measure2(rep=800, timeout=1)
     
     
